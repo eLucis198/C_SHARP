b354f482b9579e372a8e291bfa510d4909724b96
Guilherme Guedes <guilhermeguedesdaluz@gmail.com>
2019-12-16 00:23:00 -0300

    Last

--- AulaMVCSolution/ProvaBimestral2/Controllers/NotaController.cs
+++ AulaMVCSolution/ProvaBimestral2/Controllers/NotaController.cs
@@ -1,6 +1,8 @@
 ﻿using ProvaBimestral2.Models;
 using System;
 using System.Collections.Generic;
+using System.Data.Entity;
+using System.Globalization;
 using System.Linq;
 using System.Text;
 using System.Web;
@@ -17,10 +19,103 @@ namespace ProvaBimestral2.Controllers
             return View();
         }
 
-
         public ActionResult Listar()
         {
             return View();
+        }
+
+        public JsonResult SalvarNota(FormCollection form)
+        {
+            IList<ItemNota> itensNota = new List<ItemNota>();
+            if (Session["ItensNota"] != null)
+            {
+                itensNota = (List<ItemNota>)Session["itensNota"];
+            }
+
+
+            Nota nota = new Nota();
+            nota.Cliente = db.Cliente.Find(Convert.ToInt32(form["selectCliente"]));
+            nota.DataNota = DateTime.Now;
+            nota.ValorNota = Convert.ToDecimal(form["txtTotalNota"], CultureInfo.InvariantCulture);
+            nota.ItemNota = itensNota;
+            db.Nota.Add(nota);
+            db.SaveChanges();
+
+            Session.Remove("ItensNota");
+
+            return Json(new
+            {
+
+            } ,JsonRequestBehavior.AllowGet);
+        }
+
+        public JsonResult PegaNumeroNota()
+        {
+            Nota n = db.Nota.ToList().LastOrDefault();
+            int x;
+
+            if (n==null)
+            {
+                x = 1;
+            }
+            else {
+                x = n.ID_Nota + 1;
+            
+            }
+            return Json(new
+            {
+                NumeroNota = x
+            },JsonRequestBehavior.AllowGet);
+        }
+
+        public JsonResult AdicionarProdutoNota(FormCollection form)
+        {
+            IList<ItemNota> itensNota = new List<ItemNota>();
+            if (Session["ItensNota"] != null)
+            {
+                itensNota = (List<ItemNota>)Session["ItensNota"];
+            }
+
+            ItemNota item = new ItemNota();
+            item.ID_Produto = Convert.ToInt32(form["selectProduto"]);
+            item.Quantidade = Convert.ToInt32(form["txtQuantidade"]);
+            item.Valor = Convert.ToDecimal(form["txtValorUnitario"], CultureInfo.InvariantCulture);
+            item.ValorTotal = Convert.ToDecimal(form["txtValorTotal"], CultureInfo.InvariantCulture);
+
+            itensNota.Add(item);
+
+            Session["ItensNota"] = itensNota;
+
+            decimal x=0.00M;
+
+            for (int i = 0; i < itensNota.Count; i++)
+            {
+                x += (decimal)itensNota[i].ValorTotal;
+            }
+
+            return Json(new
+            {
+                ValorNota = x,
+                ListaItensNota = CriaTabelaItens(itensNota)
+            }, JsonRequestBehavior.AllowGet);
+        }
+
+        public string CriaTabelaItens(IList<ItemNota> itensNota)
+        {
+            StringBuilder str = new StringBuilder();
+
+            str.Append("<table class=\"table table-bordered\">");
+            str.Append("<tr> <td>PRODUTO</td> <td>QTD</td> <td>VALOR UNI</td> <td>VALOR TOTAL</td> </tr>");
+
+            foreach (var item in itensNota)
+            {
+                Produto p = db.Produto.Find(item.ID_Produto);
+                str.Append("<tr> <td>"+p.Descricao+"</td> <td>"+item.Quantidade+"</td> <td>"+item.Valor+"</td> <td>"+item.ValorTotal+"</td> </tr>");
+            }
+
+            str.Append("</table>");
+
+            return str.ToString();
         }
 
         public void SetaIdDetalhes(int id)
@@ -47,6 +142,7 @@ namespace ProvaBimestral2.Controllers
         public JsonResult ListaNotas()
         {
             IList<Nota> listaNotas = new List<Nota>();
+            
             listaNotas = db.Nota.ToList();
 
             StringBuilder str = new StringBuilder();
@@ -71,6 +167,7 @@ namespace ProvaBimestral2.Controllers
         public JsonResult PesquisarClientes()
         {
             IList<Cliente> listaClientes = new List<Cliente>();
+            
             listaClientes = db.Cliente.ToList();
 
             StringBuilder str = new StringBuilder();
@@ -89,6 +186,7 @@ namespace ProvaBimestral2.Controllers
         public JsonResult PesquisarProdutos()
         {
             IList<Produto> listaProdutos = new List<Produto>();
+            
             listaProdutos = db.Produto.ToList();
 
             StringBuilder str = new StringBuilder();
@@ -114,8 +212,6 @@ namespace ProvaBimestral2.Controllers
                 valor = p.Valor.ToString()
             }, JsonRequestBehavior.AllowGet);
         }
-
-        
 
     }
 }
